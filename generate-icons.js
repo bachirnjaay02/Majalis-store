83ed599840e356104bb4852a7a00d62278f88efe
BASSIROUNDIAYE233 <bachirndiaye233@gmail.com>
2026-05-07 23:33:31 +0000

    fix: regenerate PWA icons with dark background

--- generate-icons.js
+++ generate-icons.js
@@ -6,7 +6,7 @@ mkdirSync('./public/icons', { recursive: true });
 const sizes = [72, 96, 128, 144, 152, 192, 384, 512];
 
 for (const size of sizes) {
-  await sharp('./majalis-store-blanc.png')
+  await sharp('./majalis.png')
     .resize(size, size, {
       fit: 'contain',
       background: { r: 26, g: 18, b: 7, alpha: 1 }
